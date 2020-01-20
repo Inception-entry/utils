50f82b85de2e203591609c46126090e5b12a8f03
Inception-entry <batman_gotham@126.com>
2020-01-20 14:11:42 +0800

    更正引入模块的名字

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /*
  * @Author: batman
  * @Date: 2019-12-11 15:44:07
- * @LastEditors  : batman
- * @LastEditTime : 2020-01-20 10:48:21
+ * @LastEditors  : shengkesi
+ * @LastEditTime : 2020-01-20 14:05:41
  * @Description: 入口
  */
 import { formatMobile } from './src/mobile';
 import { formatTime } from './src/time';
 import { stopBubble } from './src/bubble';
-import { vilidator } from './src/validation/validator';
+import { Validator } from './src/validation/validator';
 import { uniq, uniqWith, uniqBy } from './src/handler/index';
-export { formatMobile, formatTime, stopBubble, vilidator, uniq, uniqWith, uniqBy };
+export { formatMobile, formatTime, stopBubble, Validator, uniq, uniqWith, uniqBy };
--- src/validation/validator.js
+++ src/validation/validator.js
@@ -1,8 +1,8 @@
 /*
  * @Author: batman
  * @Date: 2019-12-21 23:00:33
- * @LastEditors  : batman
- * @LastEditTime : 2020-01-01 21:42:34
+ * @LastEditors  : shengkesi
+ * @LastEditTime : 2020-01-20 14:01:38
  * @Description: 表单合法性校验,多数用于WAP，h5, app 等移动端的开发，（用ES6语法重新编写过）
  */
 Function.prototype.before = function(beforefn) {
@@ -83,7 +83,7 @@ class Validator {
 	}
 }
 
-export default Validator;
+export  {Validator};
 
 /*  调用示例 */
 /* var registerForm = document.getElementById('registerForm');
