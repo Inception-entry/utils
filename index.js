1150002fe23b582de829e438505df71adde1da5b
Inception-entry <batman_gotham@126.com>
2020-01-20 10:49:07 +0800

    edit entry file

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * @Author: batman
  * @Date: 2019-12-11 15:44:07
  * @LastEditors  : batman
- * @LastEditTime : 2020-01-01 16:00:04
+ * @LastEditTime : 2020-01-20 10:48:21
  * @Description: 入口
  */
-import {formatMobile} from './src/mobile';
-import {formatTime} from './src/time';
-import {stopBubble} from './src/bubble';
-import {vilidator} from './src/validation/validator';
-export {formatMobile, formatTime, stopBubble, vilidator};
+import { formatMobile } from './src/mobile';
+import { formatTime } from './src/time';
+import { stopBubble } from './src/bubble';
+import { vilidator } from './src/validation/validator';
+import { uniq, uniqWith, uniqBy } from './src/handler/index';
+export { formatMobile, formatTime, stopBubble, vilidator, uniq, uniqWith, uniqBy };
