180467205a68fc98eb7e11084a99ad91056aaaf3
Inception-entry <batman_gotham@126.com>
2020-05-13 16:00:17 +0800

    完善防抖动和节流工具函数

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
  * @Author: batman
  * @Date: 2019-12-11 15:44:07
  * @LastEditors: sanks
- * @LastEditTime: 2020-05-10 17:40:05
+ * @LastEditTime: 2020-05-13 14:47:58
  * @Description: 入口
  */
 import { formatMobile } from './src/mobile';
 import { formatTime } from './src/time';
 import { stopBubble } from './src/bubble';
-import { debounce } from './src/debounce_throttle';
+import { debounce, throttle } from './src/debounce_throttle';
 import { Validator } from './src/validation/validator';
 import { isEqual, uniq, uniqWith, uniqBy } from './src/handler/index';
 import { Decimal, muldivfloat, floatObj } from './src/decimal/decimal';
 
-export { formatMobile, formatTime, stopBubble, debounce, Validator, isEqual, uniq, uniqWith, uniqBy,  Decimal, muldivfloat, floatObj};
+export { formatMobile, formatTime, stopBubble, debounce, throttle, Validator, isEqual, uniq, uniqWith, uniqBy,  Decimal, muldivfloat, floatObj};
--- src/debounce_throttle.js
+++ src/debounce_throttle.js
@@ -1,19 +1,77 @@
-function debounce(cb, wait = 3000, immediate = false) {
-	let timeout;
+/*
+ * @Author: sanks
+ * @Date: 2020-05-13 08:50:16
+ * @LastEditors: sanks
+ * @LastEditTime: 2020-05-13 15:58:07
+ * @Description: 函数防抖
+ * @param func 函数
+ * @param wait 延迟执行毫秒数
+ * @param immediate true 表立即执行，false 表非立即执行
+ * @param executeOncePerWait true 时表示 throttle， 否则为 debounce
+ */
 
-	return function(...args) {
-		let callNow = immediate && !timeout;
+function debounceOrThrottle({ fn, wait = 300, immediate = false, executeOncePerWait = false }) {
+	if (typeof fn !== 'function') {
+		throw new Error('fn is expected to be a function');
+	}
 
-		if (timeout) clearTimeout(timeout);
+	let tId = null;
+	let context = null;
+	let args = null;
+	let lastTriggerTime = null;
+	let result = null;
+	let lastExecutedTime = null;
 
-		timeout = setTimeout(() => {
-			timeout = null;
+	const later = function() {
+		const last = Date.now() - (executeOncePerWait ? lastExecutedTime : lastTriggerTime);
 
-			if (!immediate) cb.apply(this, args);
-		}, wait);
+		if (last < wait && last > 0) {
+			setTimeout(later, wait - last);
+		} else {
+			if (!immediate) {
+				executeOncePerWait && (lastExecutedTime = Date.now());
+				result = fn.apply(context, args);
+				context = args = null;
+			}
 
-		if (callNow) cb.apply(this, args);
+			tId = null;
+		}
+	};
+
+	return function() {
+		context = this;
+		args = arguments;
+		!executeOncePerWait && (lastTriggerTime = Date.now());
+		const callNow = immediate && !tId;
+
+		if (!tId) {
+			executeOncePerWait && (lastExecutedTime = Date.now());
+			tId = setTimeout(later, wait);
+		}
+
+		if (callNow) {
+			executeOncePerWait && (lastExecutedTime = Date.now());
+			result = fn.apply(context, args);
+			context = args = null;
+		}
+
+		return result;
 	};
 }
 
-export {debounce};
+const debounce = ({ fn, wait, immediate }) =>
+	debounceOrThrottle({
+		fn,
+		wait,
+		immediate,
+	});
+
+const throttle = ({ fn, wait, immediate = true }) =>
+	debounceOrThrottle({
+		fn,
+		wait,
+		immediate,
+		executeOncePerWait: true,
+	});
+
+export { debounce, throttle };
