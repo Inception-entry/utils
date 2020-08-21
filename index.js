9cd77e10591ed0df7430c68cf7768f7940f7e485
Inception-entry <batman_gotham@126.com>
2020-08-21 16:18:50 +0800

    修改引用路径问题

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @Author: batman
  * @Date: 2019-12-11 15:44:07
  * @LastEditors: batman
- * @LastEditTime: 2020-05-13 14:47:58
+ * @LastEditTime: 2020-08-21 16:18:04
  * @Description: 入口
  */
 
-import { isPhone, isIOS, isAndroid, isMobile, slasher } from './common';
+import { isPhone, isIOS, isAndroid, isMobile, slasher } from './src/common';
 import { stopBubble } from './src/bubble';
 import { debounce, throttle } from './src/debounce_throttle';
 import { deepCopy, shallowCopy } from './src/clone/index';
